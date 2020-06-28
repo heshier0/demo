0852bf66a64eeea8bfbf3d116d00a1e237ba0b7d
heshier0 <heshier@126.com>
2020-06-28 21:33:53 +0800

    config

--- audio_process/hxt/hxt_client.c
+++ audio_process/hxt/hxt_client.c
@@ -6,6 +6,44 @@
 #include "utils.h"
 #include "hxt_defines.h"
 
+static char* hxt_get_api_url(const char* api)
+{
+    if(NULL == api)
+    {
+        return NULL;
+    }
+    char* server_url = hxt_get_server_url();
+    if(NULL == server_url)
+    {
+        return NULL;
+    }
+    char* ver = hxt_get_api_version();
+    if(NULL == ver)
+    {
+        utils_free(server_url);
+        return NULL;
+    }
+
+    char request_url[256] = {0};
+    strcpy(request_url, server_url);
+    strcat(request_url, "/api/");
+    strcat(request_url, ver);
+    strcat(request_url, api);
+    utils_print("%s\n", request_url);
+
+    char* ret_value = (char*)utils_calloc(strlen(request_url) + 1);
+    if(NULL == ret_value)
+    {
+        utils_free(server_url);
+        utils_free(ver);
+        return NULL;
+    }
+    strcpy(ret_value, request_url);
+    ret_value[strlen[ret_value]] = '\0';
+
+    return ret_value;
+}
+
 static void hxt_get_token_response(void* data)
 {
     if(NULL == data)
@@ -35,28 +73,17 @@ static void hxt_get_token_response(void* data)
 
 int hxt_get_token()
 {
-    char* server_url = hxt_get_server_url();
-    if(NULL == server_url)
-    {
-        goto CLEANUP6;
-    }
-    char* ver = hxt_get_api_version();
-    if(NULL == ver)
+    char* api_url = hxt_get_api_url(HXT_GET_TOKEN_URL);
+    if(NULL == api_url)
     {
         goto CLEANUP5;
     }
+
     char* uuid = hxt_get_desk_uuid(); 
     if(NULL == uuid)
     {
         goto CLEANUP4;
     }
-
-    char request_url[128] = {0};
-    strcpy(request_url, server_url);
-    strcat(request_url, "/api/");
-    strcat(request_url, ver);
-    strcat(request_url, HXT_GET_TOKEN_URL);
-    utils_print("%s\n", request_url);
 
     cJSON *root = cJSON_CreateObject();    
     if (NULL == root)
@@ -75,7 +102,7 @@ int hxt_get_token()
     PostMemCb out;
     out.memory = utils_malloc(1);
     out.size = 0;
-    if(!utils_post_json_data(request_url, json_data, &out))
+    if(!utils_post_json_data(api_url, json_data, &out))
     {
         utils_print("post data send failed\n");
         goto CLEANUP1;
@@ -90,8 +117,20 @@ CLEANUP2:
     cJSON_Delete(root);
 CLEANUP3:
     utils_free(uuid);
-CLEANUP4:    
-    utils_free(ver);
+CLEANUP4:
+    utils_free(api_url);
+CLEANUP5:
+    return 0;
+}
+
+BOOL hxt_report()
+{
+    char* api_url = hxt_get_api_url(HXT_GET_TOKEN_URL);
+    if(NULL == api_url)
+    {
+        goto CLEANUP5;
+    }
+
 CLEANUP5: 
     utils_free(server_url);
 CLEANUP6:
